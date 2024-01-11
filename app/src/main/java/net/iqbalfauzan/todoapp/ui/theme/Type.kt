fee3c45241b7d5acbf22df2037ca386412b2b6d8
Iqbal Fauzan <iqbalmf68@gmail.com>
2024-01-11 16:19:55 +0700

    adding new font

--- app/src/main/java/net/iqbalfauzan/todoapp/ui/theme/Type.kt
+++ app/src/main/java/net/iqbalfauzan/todoapp/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package net.iqbalfauzan.todoapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import net.iqbalfauzan.todoapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,14 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val Nunito = FontFamily(
+    listOf(
+        Font(R.font.nunito_light, FontWeight.Light),
+        Font(R.font.nunito_regular, FontWeight.Normal),
+        Font(R.font.nunito_medium, FontWeight.Medium),
+        Font(R.font.nunito_semibold, FontWeight.SemiBold),
+        Font(R.font.nunito_bold, FontWeight.Bold),
+    )
 )
